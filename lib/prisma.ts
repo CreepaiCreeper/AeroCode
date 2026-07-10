bf5f56f7213303e21acc6d149ea850a351e4be60
CreepaiCreeper <creeperxd8758@gmail.com>
2026-07-10 20:17:11 +0300

    fix: add prisma generate to build script

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const Prisma = new PrismaClient()
+export const Prisma = globalForPrisma.prisma || new PrismaClient();
 
-export default Prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = Prisma;
+
+export default Prisma;
